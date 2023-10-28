1edd649e220fff9062c201447a47bf3ed86eab7b
JonBay <134355923+JonBay@users.noreply.github.com>
2023-10-27 21:09:20 -0600

    finished project

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# daily-calendar
-A web page calendar that can track activities by the hour. 
+# Daily Calendar
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  This webpage will display the standard working hours of 9-5 and allow you to save events during that timeframe.  It uses jQuery to dynamically render the page. You can save To do's in each hour and they are saved to local storage.  
+
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  Clone the repository and open the index.html in a web browser.   
+
+  ## Usage
+  This website can be used to track the events in your day.  
+
+  ## License
+    
+    Licensed under the [MIT License]
+    for more information about the license see https://choosealicense.com/licenses/mit/ 
+    
+
+  ## Contributing
+  JonBay
+
+  ## Tests
+  No test instructions. 
+
+  ## Questions
+  https://github.com/JonBay
+  For questions about this project you can reach out to jon@shipidf.com.
--- script.js
+++ script.js
@@ -1,69 +1,54 @@
-var timeDisplay = $('#currentDay');
-var saveEventBtn = $('.saveBtn');
+var timeDisplay = $("#currentDay");
+var saveEventBtn = $(".saveBtn");
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
- 
-  saveEventBtn.on('click', handleSaveEvent);
+  saveEventBtn.on("click", handleSaveEvent);
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  $(document).ready(function() {
+  $(document).ready(function () {
     // Get the current hour using Day.js
     var currentHour = dayjs().hour();
-  
-    // Iterate through each time-block div
-    $('.time-block').each(function() {
-      var blockHour = parseInt($(this).attr('id').split('-')[1]);
-  
-      // Compare and add appropriate class
+
+    // Iterate through each time-block
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]); //I used internet help to figure out the split
+
+      // compare time of day and add past,present,future class
       if (blockHour < currentHour) {
-        $(this).addClass('past');
+        $(this).addClass("past");
       } else if (blockHour === currentHour) {
-        $(this).addClass('present');
+        $(this).addClass("present");
       } else {
-        $(this).addClass('future');
+        $(this).addClass("future");
       }
     });
   });
 
   function handleSaveEvent() {
-    // Get the user input and the time-block's ID
-    var userInput = $(this).siblings('.description').val();
-    var timeBlockId = $(this).closest('.time-block').attr('id');
-  
+    // Get the user input and which time block it belongs to
+    var userInput = $(this).siblings(".description").val();
+    var timeBlockId = $(this).closest(".time-block").attr("id");
+
     // Save the user input in local storage
     localStorage.setItem(timeBlockId, userInput);
   }
 
-  $(document).ready(function() {
-    // Iterate through each time-block div
-    $('.time-block').each(function() {
-      var blockId = $(this).attr('id');
+  $(document).ready(function () {
+    $(".time-block").each(function () {
+      var blockId = $(this).attr("id");
       var storedUserInput = localStorage.getItem(blockId);
-  
+
       if (storedUserInput) {
-        // Set the textarea value with the stored user input
-        $(this).find('.description').val(storedUserInput);
+        // Populate the text with the stored user input
+        $(this).find(".description").val(storedUserInput);
       }
     });
   });
 
+  function displayDate() {
+    var currentDate = dayjs().format("dddd, MMM DD, YYYY");
+    timeDisplay.text(currentDate);
+  }
 
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
+  displayDate();
+  setInterval(displayDate, 1000);
 });
-
-function displayTime() {
-  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-  timeDisplay.text(rightNow);
-}
-displayTime();
-setInterval(displayTime, 1000);
